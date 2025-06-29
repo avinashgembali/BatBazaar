90a589a080918d0324212bd73a1c1e809b5ddb12
avinashgembali <166501505+avinashgembali@users.noreply.github.com>
2025-06-29 23:10:34 +0530

    Update adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -9,7 +9,7 @@ const Order = require('../models/order');
 // Configure Multer for image upload
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, 'uploads/'),
-  filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
+  filename: (req, file, cb) => cb(null, file.originalname) 
 });
 
 const upload = multer({ storage });
