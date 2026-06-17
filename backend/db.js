d06776ed6d556743dda008ea0c11d581a5cb54ee
avinashgembali <avinashgembali05@gmail.com>
2026-06-17 22:33:14 +0530

    feat: simplify MongoDB connection by removing deprecated options and set DNS servers

--- backend/db.js
+++ backend/db.js
@@ -4,9 +4,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: {conn.connection.host}`);
   } catch (error) {
     console.error(error.message);
